81b300b4d1c445182b220a6efdb087cd4ed76efa
Conectar Wali SAS <167815566+ConectarWali@users.noreply.github.com>
2024-12-29 15:03:43 -0500

    Update middleware.py

--- utils_cws_web/middleware.py
+++ utils_cws_web/middleware.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 from functools import wraps
 
 class Middleware:
@@ -14,16 +14,16 @@ class Middleware:
         return self.__pre
 
     @pre.setter
-    def pre(self, value: Any):
-        self.__pre = value   
+    def pre(self, value: Callable):
+        self.__pre = value
 
     @property
     def post(self) -> Any:
         return self.__post
 
     @post.setter
-    def post(self, value: Any):
-        self.__post = value   
+    def post(self, value: Callable):
+        self.__post = value
 
     @classmethod
     def get_instance(cls) -> "Middleware":
@@ -32,28 +32,31 @@ class Middleware:
         return cls._instances[cls]
 
     @classmethod
-    def middleware_in(cls):
-        def decorator(function):
+    def middleware_in(cls, **decorator_kwargs):
+        def outer_decorator(function):
             @wraps(function)
             def wrapper(*args, **kwargs):
                 instance = cls.get_instance()
                 if not instance.pre:
                     raise ValueError(f"Middleware '{cls.__name__}' does not have a 'pre' function defined")
-                pre_value = instance.pre(*args, **kwargs)
+                # Call the pre function with additional decorator arguments
+                pre_value = instance.pre(*args, **kwargs, **decorator_kwargs)
                 return function(pre=pre_value, *args, **kwargs)
             return wrapper
-        return decorator
-    
+        return outer_decorator
+
     @classmethod
-    def middleware_out(cls):
-        def decorator(function):
+    def middleware_out(cls, **decorator_kwargs):
+        def outer_decorator(function):
             @wraps(function)
             def wrapper(*args, **kwargs):
                 instance = cls.get_instance()
                 if not (instance.pre and instance.post):
                     raise ValueError(f"Middleware '{cls.__name__}' does not have both 'pre' and 'post' functions defined")
-                pre_value = instance.pre(*args, **kwargs)
+                # Call the pre function with additional decorator arguments
+                pre_value = instance.pre(*args, **kwargs, **decorator_kwargs)
                 result = function(pre=pre_value, *args, **kwargs)
-                return instance.post(result)
+                # Call the post function with additional decorator arguments
+                return instance.post(result, **decorator_kwargs)
             return wrapper
-        return decorator
+        return outer_decorator
